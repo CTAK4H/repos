cd7ea02c8ec9e2cb4dc0b0748901b237f8ccfb24
CTAK4H <132345909+CTAK4H@users.noreply.github.com>
2024-03-01 17:09:01 +0500

    Add files via upload

---
+++ Section 3/3.6/3.6.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int sum = 0;
+    int num;
+
+    while (1) {
+        printf("Введите число: ");
+        scanf("%d", &num);
+
+        if (num == 0) {
+            break;
+        }
+
+        sum += num;
+        printf("Сумма: %d\n", sum);
+    }
+
+    printf("Программа завершена.\n");
+    return 0;
+}
