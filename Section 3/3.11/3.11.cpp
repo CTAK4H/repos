2ec6dffa21931316e67832c95b5e58a9323a0018
CTAK4H4IK <132345909+CTAK4H@users.noreply.github.com>
2024-03-01 22:18:08 +0500

    Add files via upload

---
+++ Section 3/3.11/3.11.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <clocale>
+
+
+
+int main() 
+{
+    setlocale(LC_ALL, "Rus");
+    int price, quantity;
+    printf("Цена одной ручки = : ");
+    scanf("%d", &price);
+    printf("Количество ручек = : ");
+    scanf("%d", &quantity);
+
+    float discountedPrice;
+    int remainingPens;
+
+    printf("Cтоимость ручек:\n");
+    for (int i = 1; i <= quantity; i++) {
+        remainingPens = i % 3;
+        discountedPrice = price * i;
+        if (remainingPens == 0) {
+            discountedPrice -= price * 0.5;
+        }
+        printf("%d ручка(и) - %.2f Рублей\n", i, discountedPrice);
+    }
+
+    return 0;
+}
---
+++ Section 3/3.9/3.9.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <clocale>
+
+
+int main() 
+{
+    setlocale (LC_ALL, "Rus");
+
+    int N;
+
+    printf("Введите число N (N < 10): ");
+    scanf("%d", &N);
+
+    if (N > 10) {
+        printf("некоректное значение N \n");
+        return 1;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            printf("%d\t", i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
