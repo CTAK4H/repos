86b86d82bade41f2284230bf81a8a5b671a8be8d
CTAK4H4IK <132345909+CTAK4H@users.noreply.github.com>
2024-03-02 14:44:41 +0500

    Add files via upload

---
+++ Section 3/3.10/3.10.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <clocale>
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int height;
+    printf("Введите чисо: ");
+    scanf("%d", &height);
+
+    for (int i = 1; i <= height; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Section 3/3.7/3.7.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+
+    int number, number2 = 0, sum, count = 0;
+
+    printf("Введите трехзначное число: ");
+
+    scanf("%d", &number);
+    do
+    {
+        sum = 0;
+        number2 = number;
+        while (number2 > 0)
+        {
+            sum += number2 % 10;
+            number2 /= 10;
+        }
+        number -= sum;
+        count++;
+        printf("%d %d\n", number, sum);
+    } while (number > 1);
+    printf("Потребовалось повторений - %d\n", count);
+}
