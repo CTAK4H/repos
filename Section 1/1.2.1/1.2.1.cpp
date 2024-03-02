7c334fe5e67d57d7bb4cf32a46674399b6b627a5
CTAK4H4IK <132345909+CTAK4H@users.noreply.github.com>
2024-03-02 14:37:49 +0500

    Add files via upload

---
+++ Section 1/1.2.1/1.2.1.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	int a, b, a_plus_b, a_minus_b;
+
+	printf("Введите число 1: ");
+	scanf("%d", &a);
+
+	printf("Введите число 2: ");
+	scanf("%d", &b);
+
+	a_plus_b = a + b;
+	printf("%d + %d = %d", a, b, a_plus_b);
+
+	a_minus_b = a - b;
+	printf("\n%d - %d = %d", a, b, a_minus_b);
+
+}
---
+++ Section 1/1.2.2/1.2.2.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	float a, b;
+
+	printf("Введите число 2 числа через пробел: ");
+	scanf("%f %f", &a, &b);
+
+	printf("%f + %f = %f", a, b, a + b);
+
+	printf("\n%f - %f = %f", a, b, a - b);
+
+	printf("\n%f / %f = %f", a, b, a / b);
+
+	printf("\n%f * %f = %f", a, b, a * b);
+}
