a48ec7d9f361a2d939200dfb9f4ecfb361a6d419
CTAK4H4IK <132345909+CTAK4H@users.noreply.github.com>
2024-03-02 15:12:41 +0500

    Add files via upload

---
+++ Section 4/4.1/4.1.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <clocale>
+
+int main() 
+{
+	setlocale(LC_ALL, "Rus");
+	int mas[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int n = sizeof(mas) / sizeof(mas[0]);
+
+	printf("Чётных чисел в массиве: ");
+	for (int i = 0; i < n; i++) 
+	{
+		if (mas[i] % 2 == 0) 
+		{
+			printf("%d ", mas[i]);
+		}
+	}
+	return 0;
+}
