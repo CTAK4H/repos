01034770060b8fc301f2f3e5bbbd15962d31a72b
CTAK4H4IK <132345909+CTAK4H@users.noreply.github.com>
2024-03-02 14:42:49 +0500

    Add files via upload

---
+++ Section 3/3.4/3.4.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <clocale>
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+
+    int n, m, i, k, f;
+
+    printf("Введите первое число: ");
+    scanf("%d", &n);
+    printf("Введите второе число: ");
+    scanf("%d", &m);
+    printf("Введите делитель: ");
+    scanf("%d", &k);
+
+    f = 0;
+
+    for (i = n; i <= m; i++) {
+        if (i % k == 0) {
+            f = 1;
+            printf("%d ", i);
+        }
+    }
+
+    if (!f) {
+        printf("В этом диапазоне таких чисел нет");
+    }
+
+    return 0;
+}
