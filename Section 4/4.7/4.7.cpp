e9ac9d28f573037defee1c7d83013b5566f58734
CTAK4H4IK <132345909+CTAK4H@users.noreply.github.com>
2024-03-03 21:53:07 +0500

    Add files via upload

---
+++ Section 4/4.7/4.7.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int arr[5] = { 1, 2, 3, 4, 5 };
+    int i, user_input, count = 0;
+
+    printf("Введите число (Максимально - 5): ");
+    scanf("%d", &user_input);
+
+    for (i = 0; i < 5; i++) {
+        if (arr[i] > user_input) {
+            count++;
+        }
+    }
+
+    printf("Количество чисел в массиве, которые больше введенного числа: %d\n", count);
+
+    return 0;
+}
---
+++ Section 4/4.8/4.8.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int arr[5] = { 1, 2, 3, 4, 5 };
+    int i, sum = 0;
+
+    for (i = 0; i < 5; i++) {
+        sum += arr[i];
+    }
+
+    float average = sum / 5.0;
+
+    printf("Среднее значение чисел массива: %f\n", average);
+
+    return 0;
+}
