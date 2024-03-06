9a58d18f7f4aa6a12e73bf05c601fa880757d368
CTAK4H4IK <132345909+CTAK4H@users.noreply.github.com>
2024-03-06 11:44:33 +0500

    Add files via upload

---
+++ 1.1/1.1.cpp
@@ -0,0 +1,71 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+#include<conio.h>
+
+int main()
+{
+
+	setlocale(LC_ALL, "Rus");
+	int a, sum, coffe, Chocolate, chips_lite, chips_big;
+	while (coffe = 1)
+	{
+
+
+		printf("Вставьте купюру или оплатите банковской картой \n");
+		printf("Введите номинал вложенной суммы: ");
+		scanf("%d", &sum);
+
+		system("cls");
+		printf("Ваш баланс %d \n", sum);
+
+		printf("Доступные товары:\n Кофе №1 - 100 рублей\n Шоколадный батончик №2 - 150 рублей \n Чипсы (80 Грамм) №3 - 80 рублей \n Чипсы 120 (Грамм) №4 - 120 рублей \n ");
+
+		printf(" Введите код товара: ");
+		scanf("%d", &a);
+
+		system("cls");
+
+		printf("Ваш баланс %d", sum);
+
+		if (a == 1 || (sum >= 100)) 
+		{
+			printf("Возьмите ваш товар\n");
+		}
+		else if (sum <= 100) 
+		{
+			printf("Недостаточно денег");
+		}
+
+		else if (a == 2 || (sum >= 150)) 
+		{
+			printf("Возьмите ваш товар\n");
+		}
+		else if (sum <= 150) {
+			printf("Недостаточно денег");
+		}
+
+		else if (a == 3 || (sum >= 80)) {
+			printf("Возьмите ваш товар\n");
+		}
+		else if (sum <= 80) {
+			printf("Недостаточно денег");
+		}
+
+		else if (a == 4 || (sum >= 120)) {
+			printf("Возьмите ваш товар\n");
+		}
+		else if (sum <= 120) {
+			printf("Недостаточно денег");
+		}
+
+		else {
+			printf("Неверный код товара");
+		}
+		system("cls");
+	}
+
+	
+
+}
